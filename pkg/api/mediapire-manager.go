74e382c2b0479c6f0f017dbffc66987ca76392f5
ericTurf <eric.turriff@gmail.com>
2022-09-04 12:16:04 -0400

    add connection info to client

--- pkg/api/mediapire-manager.go
+++ pkg/api/mediapire-manager.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"net"
 	"net/http"
 
 	"github.com/egfanboy/mediapire-manager/pkg/types"
@@ -14,7 +15,15 @@ type MediaManagerApi interface {
 	RegisterNode(r types.RegisterNodeRequest) (*http.Response, error)
 }
 
-type managerClient struct{}
+type ManagerConnectionConfig struct {
+	Host   net.IP
+	Scheme string
+	Port   *int
+}
+
+type managerClient struct {
+	connection ManagerConnectionConfig
+}
 
 func (c *managerClient) RegisterNode(registerRequest types.RegisterNodeRequest) (r *http.Response, err error) {
 	var buf bytes.Buffer
@@ -26,22 +35,21 @@ func (c *managerClient) RegisterNode(registerRequest types.RegisterNodeRequest) 
 	port := 443
 
 	if registerRequest.Port != nil {
-		port = *registerRequest.Port
+		port = *c.connection.Port
 	}
 
-	r, err = http.Post(
-		fmt.Sprintf(
-			"%s://%s:%d/api/v1/nodes/register",
-			registerRequest.Scheme,
-			registerRequest.Host.String(),
-			port,
-		),
+	r, err = http.Post(fmt.Sprintf(
+		"%s://%s:%d/api/v1/nodes/register",
+		c.connection.Scheme,
+		c.connection.Host.String(),
+		port,
+	),
 		"application/json",
 		&buf)
 
 	return
 }
 
-func NewManagerClient(ctx context.Context) MediaManagerApi {
-	return &managerClient{}
+func NewManagerClient(ctx context.Context, connection ManagerConnectionConfig) MediaManagerApi {
+	return &managerClient{connection: connection}
 }
