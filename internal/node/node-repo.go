794c95f1bad3190022b681f75ef909b0f74508b1
ericTurf <eric.turriff@gmail.com>
2022-09-05 10:45:09 -0400

    added API to stream file from node

--- internal/node/node-repo.go
+++ internal/node/node-repo.go
@@ -2,6 +2,7 @@ package node
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/egfanboy/mediapire-manager/internal/app"
 	"github.com/egfanboy/mediapire-manager/internal/constants"
@@ -9,13 +10,14 @@ import (
 
 type NodeRepo interface {
 	GetAllNodes(ctx context.Context) ([]NodeConfig, error)
+	GetNode(ctx context.Context, nodeId string) (NodeConfig, error)
 }
 
 type redisRepo struct {
 	app *app.App
 }
 
-func (r redisRepo) GetAllNodes(ctx context.Context) (result []NodeConfig, err error) {
+func (r *redisRepo) GetAllNodes(ctx context.Context) (result []NodeConfig, err error) {
 	// Get number of hosts saved
 	cmd := r.app.Redis.LLen(ctx, constants.KeyListHosts)
 	if cmd.Err() != nil {
@@ -32,17 +34,7 @@ func (r redisRepo) GetAllNodes(ctx context.Context) (result []NodeConfig, err er
 	}
 
 	for _, h := range rangeCmd.Val() {
-
-		hgetCmd := r.app.Redis.HGetAll(ctx, MakeNodeHash(h))
-
-		//  partial failures
-		if hgetCmd.Err() != nil {
-			// handle error
-
-		}
-
-		var nodeModel NodeConfig
-		err := hgetCmd.Scan(&nodeModel)
+		nodeModel, err := r.GetNode(ctx, h)
 		if err != nil {
 			// handle error
 		} else {
@@ -54,6 +46,29 @@ func (r redisRepo) GetAllNodes(ctx context.Context) (result []NodeConfig, err er
 	return
 }
 
+func (r *redisRepo) GetNode(ctx context.Context, nodeId string) (NodeConfig, error) {
+	hgetCmd := r.app.Redis.HGetAll(ctx, MakeNodeHash(nodeId))
+
+	//  partial failures
+	if hgetCmd.Err() != nil {
+		// handle error
+		return NodeConfig{}, hgetCmd.Err()
+	}
+
+	var nodeModel NodeConfig
+	err := hgetCmd.Scan(&nodeModel)
+
+	if err != nil {
+		return NodeConfig{}, err
+	}
+
+	if nodeModel.NodeHost == "" || nodeModel.NodePort == "" || nodeModel.NodeScheme == "" {
+		return nodeModel, fmt.Errorf("no node found with id %s", nodeId)
+	}
+
+	return nodeModel, nil
+}
+
 func NewNodeRepo() NodeRepo {
-	return redisRepo{app: app.GetApp()}
+	return &redisRepo{app: app.GetApp()}
 }
